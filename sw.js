a1fc52bc203e5cdef2bdaa00ec33f549da4558fa
Theodore Song <johnsong10086@hotmail.com>
2026-08-18 12:54:16 -0400

    Preserve adaptive signals until grading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "polymarket-arena-build-51";
+const CACHE_NAME = "polymarket-arena-build-52";
 const APP_SHELL = ["/", "/index.html", "/personal.html", "/cycle-worker.js"];
 
 self.addEventListener("install", event => {
