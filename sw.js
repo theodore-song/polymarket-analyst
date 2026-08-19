d7c6b8aa5e4c82216f698a2bf10624248e78a3ce
Theodore Song <johnsong10086@hotmail.com>
2026-08-19 16:13:46 -0400

    Refresh offline cache for build 68

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "polymarket-arena-build-67";
+const CACHE_NAME = "polymarket-arena-build-68";
 const APP_SHELL = ["/", "/index.html", "/personal.html", "/cycle-worker.js"];
 
 self.addEventListener("install", event => {
