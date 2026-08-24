7189e9219639d6f191b7ced0e297655e4c4114da
Theodore Song <johnsong10086@hotmail.com>
2026-08-24 14:25:39 -0400

    Normalize protected maker learning returns

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "polymarket-arena-build-155";
+const CACHE_NAME = "polymarket-arena-build-156";
 const APP_SHELL = ["/", "/index.html", "/personal.html", "/cycle-worker.js"];
 
 self.addEventListener("install", event => {
